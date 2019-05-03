1d9abcbccbc356564d084246af3c0567b3de12a4
unknown <gustanik@student.rtc.edu>
2019-05-02 23:09:06 -0700

    Last Python

---
+++ turtlesWoComments.py
@@ -0,0 +1,40 @@
+import turtle
+import random
+
+wn = turtle.Screen()
+wn.bgcolor('lightblue')
+wn.title("Turtle Race")
+
+lance = turtle.Turtle()
+andy = turtle.Turtle()
+lance.color('red')
+andy.color('blue')
+lance.shape('turtle')
+andy.shape('turtle')
+
+andy.up()
+lance.up()
+andy.goto(-100, 20)
+lance.goto(-100, -20)
+
+start = turtle.Turtle()  # OPTIONAL-
+start.hideturtle()
+
+for i in range(150):
+
+    andyDistance = random.randrange(1, 5)
+    lanceDistance = random.randrange(1, 5)
+    andy.forward(andyDistance)
+    lance.forward(lanceDistance)
+    andyTotalDistance = andyTotalDistance + andyDistance
+    lanceTotalDistance = lanceTotalDistance + lanceDistance
+
+
+for eachInt in range(145):
+    if andyTotalDistance > lanceTotalDistance:
+        start.write("Andy is the winner!", move=False, align="center", font=("Arial", 25, "normal"))
+    elif lanceTotalDistance > andyTotalDistance:
+        start.write("Lance is the winner!", move=False, align="center", font=("Arial", 25, "normal"))
+    else: print("Tie Game")
+
+wn.exitonclick()
